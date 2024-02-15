f851de9bef99470f0ac112edde7fcae7800f8f4f
Morty052 <anomaly619052@gmail.com>
2024-02-15 00:59:43 -0800

    touching from windows frist time

--- src/components/addproduct-table/index.tsx
+++ src/components/addproduct-table/index.tsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import { Button } from '../ui/button'
-import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from '../ui/table'
 
 interface IProduct {
   name: string
@@ -18,8 +16,21 @@ export function AddProductTable({ products }: Props) {
     name: '',
     image: '',
     description: '',
-    price: '',
+    price: 0,
   })
+
+  const [AddedProducts, setAddedproducts] = useState<[] | IProduct[]>([])
+
+  const handleAddProduct = () => {
+    console.log(newProduct)
+    setAddedproducts((prev) => [...prev, newProduct])
+    setNewProduct({
+      name: '',
+      image: '',
+      description: '',
+      price: 0,
+    })
+  }
 
   const handleTextChange = (name: any, text: string) => {
     setNewProduct((prev) => ({
@@ -28,64 +39,98 @@ export function AddProductTable({ products }: Props) {
     }))
   }
 
+  const handleFileChange = (e: any) => {
+    console.log(e.target.files[0])
+    setNewProduct((prev) => ({
+      ...prev,
+      image: URL.createObjectURL(e.target.files[0]),
+    }))
+  }
+
+  // return (
+  //   <div>
+  //     <Table>
+  //       <TableCaption>Add new product.</TableCaption>
+  //       <TableHeader>
+  //         <TableRow>
+  //           <TableHead className=" text-white">Name</TableHead>
+  //           <TableHead className=" text-white">Image</TableHead>
+  //           <TableHead className=" text-white">Description</TableHead>
+  //           <TableHead className=" text-white">Price</TableHead>
+  //           <TableHead className=" text-white">Preview</TableHead>
+  //         </TableRow>
+  //       </TableHeader>
+  //       <TableBody className=" divide-y">
+  //         {AddedProducts?.map((product) => (
+  //           <TableRow key={product.name} className="">
+  //             <TableCell className=" text-white">{product.name}</TableCell>
+  //             <TableCell className=" text-white">{product.image}</TableCell>
+  //             <TableCell className=" text-white">{product.description}</TableCell>
+  //             <TableCell className=" text-white">{product.price}</TableCell>
+  //           </TableRow>
+  //         ))}
+  //         <TableRow className="">
+  //           <TableCell className="border-x  text-white">
+  //             <input
+  //               value={newProduct.name}
+  //               onChange={(e) => handleTextChange(e.target.name, e.target.value)}
+  //               name="name"
+  //               className="rounded-lg p-2 text-black"
+  //               placeholder="Product Name"
+  //               type="text"
+  //             />
+  //           </TableCell>
+  //           <TableCell className="w-72 max-w-[250px] border-x text-white">
+  //             {!newProduct.image ? (
+  //               <input onChange={(e) => handleFileChange(e)} placeholder="file" type="file" />
+  //             ) : (
+  //               <img src={newProduct.image} className="w-full" />
+  //             )}
+  //           </TableCell>
+  //           <TableCell className=" w-full border-x text-white">
+  //             <textarea
+  //               value={newProduct.description}
+  //               name="description"
+  //               rows={5}
+  //               className=" w-full rounded-lg p-2 text-black"
+  //               placeholder="product description"
+  //               onChange={(e) => handleTextChange(e.target.name, e.target.value)}
+  //             />
+  //           </TableCell>
+  //           <TableCell className="border-x text-right text-white">
+  //             <input
+  //               name="price"
+  //               value={newProduct.price}
+  //               onChange={(e) => handleTextChange(e.target.name, e.target.value)}
+  //               className="rounded-lg p-2 text-black"
+  //               placeholder="price"
+  //               type="text"
+  //             />
+  //           </TableCell>
+  //           <TableCell className=" border-x text-right text-white">
+  //             <Button onClick={() => handleAddProduct()} className="bg-white text-black">
+  //               Preview{' '}
+  //             </Button>
+  //           </TableCell>
+  //         </TableRow>
+  //       </TableBody>
+  //     </Table>
+  //   </div>
+  // )
+
   return (
-    <div>
-      <Table>
-        <TableCaption>Add new product.</TableCaption>
-        <TableHeader>
-          <TableRow>
-            <TableHead className=" text-white">Name</TableHead>
-            <TableHead className=" text-white">Image</TableHead>
-            <TableHead className=" text-white">Description</TableHead>
-            <TableHead className=" text-white">Price</TableHead>
-            <TableHead className=" text-white">Preview</TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody className=" divide-y">
-          {/* {products?.map((product) => (
-          ))} */}
-          <TableRow className="">
-            <TableCell className="border-x  text-white">
-              <input
-                value={newProduct.name}
-                onChange={(e) => handleTextChange(e.target.name, e.target.value)}
-                name="name"
-                className="rounded-lg p-2 text-black"
-                placeholder="Product Name"
-                type="text"
-              />
-            </TableCell>
-            <TableCell className="max-w-[250px] border-x text-white">
-              <input placeholder="file" type="file" />
-            </TableCell>
-            <TableCell className=" w-full border-x text-white">
-              <textarea
-                value={newProduct.description}
-                name="description"
-                rows={5}
-                className=" w-full rounded-lg p-2 text-black"
-                placeholder="product description"
-                onChange={(e) => handleTextChange(e.target.name, e.target.value)}
-              />
-            </TableCell>
-            <TableCell className="border-x text-right text-white">
-              <input
-                name="price"
-                value={newProduct.price}
-                onChange={(e) => handleTextChange(e.target.name, e.target.value)}
-                className="rounded-lg p-2 text-black"
-                placeholder="price"
-                type="text"
-              />
-            </TableCell>
-            <TableCell className=" border-x text-right text-white">
-              <Button onClick={() => console.log(newProduct)} className="bg-white text-black">
-                Preview{' '}
-              </Button>
-            </TableCell>
-          </TableRow>
-        </TableBody>
-      </Table>
+    <div className="">
+      <div className="mx-auto grid max-w-2xl">
+        <p className="text-white">Add new product.</p>
+        <label htmlFor="">Enter Product Name</label>
+        <input type="text" className="text-white" />
+        <label htmlFor="">Select Product Image</label>
+        <input type="file" className="text-white" />
+        <label htmlFor="">Enter Product Description</label>
+        <input type="text" className="text-white" />
+        <label htmlFor="">Enter Product Price</label>
+        <input type="text" className="text-white" />
+      </div>
     </div>
   )
 }
